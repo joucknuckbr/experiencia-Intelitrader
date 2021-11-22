032d795243087cb2c94400b288228800c2b0b3ad
joucknuckbr <joucknuckbr@gmail.com>
2021-11-22 12:51:50 -0300

    Criação dos métodos e começo do log

---
+++ source/repos/CadastroApi/CadastroApi/Models/RegistrationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroApi.Models
+{
+    public class RegistrationContext : DbContext
+    {
+        public RegistrationContext(DbContextOptions<RegistrationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ source/repos/CadastroApi/CadastroApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CadastroApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
